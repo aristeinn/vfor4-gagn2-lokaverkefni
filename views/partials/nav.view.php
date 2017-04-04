a2c606428c76069ab927df3235eee8c1e48f0dab
Ari Steinn Guðmundsson <FS399820@fss.is>
2017-04-04 10:18:25 +0000

    yoo

--- views/partials/nav.view.php
+++ views/partials/nav.view.php
@@ -1,2 +1,14 @@
-<a href="/">Home</a>
-<a href="about">About</a>
+<!-- <a href="/">Home</a>
+<a href="about">About</a> -->
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/">aristeinn</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="/">Home</a></li>
+      <li><a href="about">About</a></li>
+    </ul>
+  </div>
+</nav>
